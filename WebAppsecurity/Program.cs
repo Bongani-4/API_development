0aca8237ca32de3ee2227b3f77e962990647f00e
Bongani-4 <140083292+Bongani-4@users.noreply.github.com>
2023-08-31 16:02:24 +0200

    Add files via upload

--- WebAppsecurity/Program.cs
+++ WebAppsecurity/Program.cs
@@ -1,16 +1,8 @@
-//using 35016752_WebAPI.Models;
-using Microsoft.EntityFrameworkCore;
-using _35016752_WebAPI.Models;
 var builder = WebApplication.CreateBuilder(args);
-  
+
 // Add services to the container.
 
 builder.Services.AddControllers();
-
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ProductsDBContext>(options => options.UseSqlServer(connectionString));
-
-
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
